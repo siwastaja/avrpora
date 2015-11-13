5509c33f9e2d4c866637b485703f1394d05fbf78
Antti Alhonen <antti.alhonen@tut.fi>
2015-11-13 12:51:35 +0200

    Perfect and finished but has a bug.

--- avrpora.c
+++ avrpora.c
@@ -6,7 +6,8 @@
 #include <avr/interrupt.h>
 #include <avr/sfr_defs.h>
 #include <util/delay.h>
-//#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define cbi(x,y) x &= ~(1<<(y))
 #define sbi(x,y) x |= (1<<(y))
@@ -30,10 +31,10 @@
 // 1989 mils = 4000 x steps
 // 1996 mils = 4000 y steps
 
-
-#define MAX_X 12000
-#define MAX_Y 14000
-#define MAX_Z 10000
+// Max 32767
+#define MAX_X 24100
+#define MAX_Y 24000
+#define MAX_Z 10700
 
 volatile int16_t cur_x, cur_y, cur_z;
 volatile uint8_t cur_x_dir, cur_y_dir, cur_z_dir;
@@ -61,8 +62,8 @@ volatile uint16_t y_dec_left;
 #define X_DIR_NEG() sbi(PORTF, 3)
 #define Y_DIR_POS() cbi(PORTF, 4)
 #define Y_DIR_NEG() sbi(PORTF, 4)
-#define Z_DIR_POS() sbi(PORTF, 5)
-#define Z_DIR_NEG() cbi(PORTF, 5)
+#define Z_DIR_POS() cbi(PORTF, 5)
+#define Z_DIR_NEG() sbi(PORTF, 5)
 
 #define STOP_LED_ON()  cbi(PORTA, 4)
 #define STOP_LED_OFF() sbi(PORTA, 4)
@@ -72,6 +73,22 @@ volatile uint16_t y_dec_left;
 
 #define Z_LED_ON() cbi(PORTG, 1)
 #define Z_LED_OFF() sbi(PORTG, 1)
+
+#define BUT(port, pin) ((~(port)) & (1<<(pin)))
+
+#define XNEG() BUT(PINA, 6)
+#define XPOS() BUT(PING, 2)
+#define YNEG() BUT(PINC, 6)
+#define YPOS() BUT(PINC, 4)
+#define ZBUT() BUT(PING, 0)
+#define KESKI() BUT(PINC, 3)
+#define TEACH() BUT(PINC, 2)
+#define XHOME() BUT(PINC, 1)
+#define YHOME() BUT(PINC, 0)
+#define ZHOME() BUT(PINE, 3)
+#define GOTOBUT() BUT(PINC, 5)
+#define RESETBUT() BUT(PINA, 5)
+#define PAUSEBUT() BUT(PINA, 7)
 
 
 void x_dir(uint8_t pos)
@@ -156,6 +173,26 @@ void wait_step()
 	while(x_steps_left || y_steps_left) ;
 }
 
+uint8_t in_pause;
+
+void wait_step_pause()
+{
+	uint8_t i;
+	while(x_steps_left || y_steps_left)
+	{
+		if(!in_pause && PAUSEBUT() && PAUSEBUT())
+		{
+			for(i = 0; i < 10; i++)
+			{
+				_delay_ms(10);
+				while(PAUSEBUT()) ;
+			}
+			// Now in pause
+			PAUSE_LED_ON();
+			in_pause = 1;
+		}
+	}
+}
 
 void step_x_1()
 {
@@ -229,7 +266,26 @@ void start_stepping(uint32_t steps_x, uint32_t steps_y)
 
 }
 
-void move_xy(int16_t x_mils, int16_t y_mils)
+
+void error()
+{
+	while(1)
+	{
+		STOP_LED_ON();
+		_delay_ms(300);
+		STOP_LED_OFF();
+		_delay_ms(200);
+		if(RESETBUT())
+		{
+			_delay_ms(10);
+			if(RESETBUT())
+				break;
+		}
+	}
+	// todo: reset state, i.e., empty buffer.
+}
+
+void move_xy(uint8_t absolute, int16_t x_mils, int16_t y_mils)
 {
 	int32_t x_tmp, y_tmp;
 
@@ -241,6 +297,21 @@ void move_xy(int16_t x_mils, int16_t y_mils)
 	y_tmp *= 40000;
 	y_tmp /= 19960;
 
+	if(absolute)
+	{
+		x_tmp -= cur_x;
+		y_tmp -= cur_y;
+	}
+
+	if((x_tmp + cur_x) < 0 || (x_tmp + cur_x) > MAX_X || (y_tmp + cur_y) < 0 || (y_tmp + cur_y) > MAX_Y)
+	{
+		error();
+		return;
+
+	}
+
+	cur_x += x_tmp;
+	cur_y += y_tmp;
 
 	if(x_tmp < 0) { x_tmp *= -1; x_dir(0);} else x_dir(1);
 	if(y_tmp < 0) { y_tmp *= -1; y_dir(0);} else y_dir(1);
@@ -248,19 +319,42 @@ void move_xy(int16_t x_mils, int16_t y_mils)
 	start_stepping(x_tmp, y_tmp);
 }
 
+uint16_t conv_cur_x()
+{
+	uint32_t tmp;
+	tmp = cur_x;
+	tmp *= 19890;
+	tmp /= 40000;
+	return tmp+1; // +1 for rounding error
+}
+
+uint16_t conv_cur_y()
+{
+	uint32_t tmp;
+	tmp = cur_y;
+	tmp *= 19960;
+	tmp /= 40000;
+	return tmp+1;
+}
 
 #define Z_ACC_STEPS 60
 
-void step_z(uint16_t steps, uint8_t down, uint8_t interval)
+void step_z(int16_t steps, uint8_t down, uint8_t interval)
 {
 	uint16_t acc_steps, dec_steps;
+
+	if(down && (cur_z + steps) > MAX_Z)
+		{error(); return;}
+
+	if(!down && (cur_z - steps) < 0)
+		{error(); return;}
 
 	if(interval < Z_ACC_STEPS+10)
 		interval = Z_ACC_STEPS+10;
 	if(interval > 250)
 		interval = 250;
 
-	if(down) Z_DIR_NEG(); else Z_DIR_POS();
+	z_dir(down);
 
 	if(steps < Z_ACC_STEPS*2)
 	{
@@ -290,20 +384,6 @@ void step_z(uint16_t steps, uint8_t down, uint8_t interval)
 	}
 }
 
-
-#define BUT(port, pin) ((~(port)) & (1<<(pin)))
-
-#define XNEG() BUT(PINA, 6)
-#define XPOS() BUT(PING, 2)
-#define YNEG() BUT(PINC, 6)
-#define YPOS() BUT(PINC, 4)
-#define ZBUT() BUT(PING, 0)
-#define KESKI() BUT(PINC, 3)
-#define TEACH() BUT(PINC, 2)
-#define XHOME() BUT(PINC, 1)
-#define YHOME() BUT(PINC, 0)
-#define ZHOME() BUT(PINE, 3)
-#define GOTOBUT() BUT(PINC, 5)
 
 void find_home_xy()
 {
@@ -354,17 +434,18 @@ void find_home_z()
 {
 	if(ZHOME())
 	{
-		Z_DIR_NEG();
+		Z_DIR_POS();
 		while(ZHOME())
 		{
 			step_z_1();
 			_delay_us(800);
 		}
+		cur_z = 1000;
 		step_z(200, 1, 100);
 		_delay_ms(50);
 	}
 
-	Z_DIR_POS();
+	Z_DIR_NEG();
 	while(!ZHOME())
 	{
 		step_z_1();
@@ -372,10 +453,11 @@ void find_home_z()
 	}
 
 	_delay_ms(50);
+	cur_z = 1000;
 	step_z(200, 1, 100);
 	_delay_ms(50);
 
-	Z_DIR_POS();
+	Z_DIR_NEG();
 
 	while(!ZHOME())
 	{
@@ -383,7 +465,7 @@ void find_home_z()
 		_delay_us(5000);
 	}
 
-	cur_z = 200;
+	cur_z = 500;
 
 }
 
@@ -415,37 +497,129 @@ char* find_c(char* str, char c)
 	}
 }
 
-ISR(USART_RXC_vect)
+
+#define print_char(c) {while((UCSR1A & 0b00100000) == 0) ; UDR1 = (c);}
+
+void print_string(char* str)
 {
-	char byte = UDR;
+	while(str[0] != 0)
+	{
+		print_char(str[0]);
+		str++;
+	}
+}
+
+#define OP_NOP 0
+#define OP_MA  1
+#define OP_MR  2
+#define OP_DRI 3
+#define OP_FZ  4
+#define OP_FH  5
+
+volatile uint8_t next_op = OP_NOP;
+volatile int16_t next_x, next_y, next_z;
+
+// DRIx,y,z  x = penetration, y = down speed, z = up speed
+// Minimum speed = 250, maximum speed = 70
+// Defaults: 1000, 130, 80
+
+ISR(USART1_RX_vect)
+{
+	char byte = UDR1;
 	if(byte == ';')
 	{
-		// rx_point holds the last position.
 		rx_buf[rx_point] = 0;
 		char* p_val;
 		if((p_val = comp_str(rx_buf, "MA")))
 		{
 			int16_t x = atoi(p_val);
-			if(p_val = find_c(p_val, ','))
+			if((p_val = find_c(p_val, ',')))
 			{
+				p_val++;
 				int16_t y = atoi(p_val);
-				if(x < 0 || x > 12000 || y < 0 || y > 12000)
+				if(x < 0 || x > 20000 || y < 0 || y > 20000)
 				{
-					
+					error();
+				}
+				else
+				{
+					next_x = x;
+					next_y = y;
+					next_op = OP_MA;
 				}
 			}
-			if(new_setpoint < -100 || new_setpoint > 200)
-			{
-				print_string("\r\nSET CMD OUT OF RANGE\r\n");
-			}
-			else
-			{
-				temp_setpoint = K_AT_0C+new_setpoint*10;
-			}
 		}
-		else if(comp_str(rx_buf, "MR"))
+		else if((p_val = comp_str(rx_buf, "MR")))
 		{
+			int16_t x = atoi(p_val);
+			if((p_val = find_c(p_val, ',')))
+			{
+				p_val++;
+				int16_t y = atoi(p_val);
+				if(x < -20000 || x > 20000 || y < -20000 || y > 20000)
+				{
+					error();
+				}
+				else
+				{
+					next_x = x;
+					next_y = y;
+					next_op = OP_MR;
+				}
+			}
+
 		}
+		else if((p_val = comp_str(rx_buf, "DRI")))
+		{
+			int16_t x=1000;
+			int16_t y=130;
+			int16_t z=80;
+
+			x = atoi(p_val);
+			if(x < 50 || x > 3000)
+				x = 1000;
+
+			if((p_val = find_c(p_val, ',')))
+			{
+				p_val++;
+				y = atoi(p_val);
+				if(y < 50 || y > 255)
+					 y = 130;
+
+				if((p_val = find_c(p_val, ',')))
+				{
+					p_val++;
+					z = atoi(p_val);
+					if(z < 50 || z > 255)
+						z = 80;
+				}
+			}
+			next_x = x;
+			next_y = y;
+			next_z = z;
+			next_op = OP_DRI;
+
+		}
+		else if(comp_str(rx_buf, "OA"))
+		{
+			char buf[10];
+			utoa(conv_cur_x(), buf, 10);
+			print_string(buf);
+			print_char(',');
+			utoa(conv_cur_y(), buf, 10);
+			print_string(buf);
+			print_char('\n');
+			print_char('\r');
+		}
+		else if(comp_str(rx_buf, "FZ"))
+		{
+			next_op = OP_FZ;
+		}
+		else if(comp_str(rx_buf, "FH"))
+		{
+			next_op = OP_FH;
+		}
+
 		rx_point = 0;
 	}
 	else
@@ -457,11 +631,21 @@ ISR(USART_RXC_vect)
 		if(rx_point >= RX_BUF_SIZE)
 			rx_point = 0;
 	}
+
+//	UDR1 = byte;
+}
+
+void drill(int16_t penetration, uint8_t down_speed, uint8_t up_speed)
+{
+	step_z(penetration, 1, down_speed);
+	_delay_ms(70);
+	step_z(penetration, 0, up_speed);
 }
 
 int main()
 {
 	uint8_t move_z = 0;
+	uint8_t nap_pained = 0;
 	TCCR1A = 0;
 	TCCR3A = 0;
 
@@ -478,21 +662,32 @@ int main()
 	TCCR1B = 0b01001; // Clear-On-CompareMatch, No prescaler, timer on.
 	TCCR3B = 0b01001; // Clear-On-CompareMatch, No prescaler, timer on.
 
+	UCSR1B = 0b10011000;
+	UCSR1C = 0b00100100;
+	UBRR1L = 47;
+	// 7-bit, even parity, 9600 bps, 1 stop bit.
+
 	sei();
+	PAUSE_LED_ON();
+	STOP_LED_ON();
+	Z_LED_ON();
+	_delay_ms(200);
+
 	PAUSE_LED_OFF();
 	STOP_LED_OFF();
 	Z_LED_OFF();
 	_delay_ms(100);
 
-	find_home_xy();
 	find_home_z();
+	find_home_xy();
 
 	while(1)
 	{
+		nap_pained = 0;
 		if(KESKI() && GOTOBUT())
 		{
-			find_home_xy();
 			find_home_z();
+			find_home_xy();
 		}
 
 		if(XNEG() && !move_z)
@@ -501,6 +696,7 @@ int main()
 			{
 				x_dir(0);
 				step_x_1();
+				nap_pained = 1;
 			}
 		}
 		else if(XPOS() && !move_z)
@@ -509,6 +705,7 @@ int main()
 			{
 				x_dir(1);
 				step_x_1();
+				nap_pained = 1;
 			}
 		}
 
@@ -516,34 +713,36 @@ int main()
 		{
 			if(move_z)
 			{
-				if(cur_z > 0)
+				if(cur_z < MAX_Z)
 				{
-					z_dir(0);
+					z_dir(1);
 					step_z_1();
-					cur_z--;
+					nap_pained = 1;
 				}
 			}
 			else if(cur_y > 0)
 			{
 				y_dir(0);
 				step_y_1();
+				nap_pained = 1;
 			}
 		}
 		else if(YPOS())
 		{
 			if(move_z)
 			{
-				if(cur_z < MAX_Z)
+				if(cur_z > 0)
 				{
-					z_dir(1);
+					z_dir(0);
 					step_z_1();
-					cur_z++;
+					nap_pained = 1;
 				}
 			}
 			else if(cur_y < MAX_Y)
 			{
 				y_dir(1);
 				step_y_1();
+				nap_pained = 1;
 			}
 		}
 
@@ -564,17 +763,62 @@ int main()
 			_delay_ms(10);
 		}
 
-		if(TEACH())
+		if(in_pause && PAUSEBUT())
 		{
-			step_z(1000, 1, 130);
-			_delay_ms(100);
-			step_z(1000, 0, 80);
+			in_pause = 0;
+			PAUSE_LED_OFF();
+			print_string("OK\n\r");
 		}
 
-		if(KESKI())
-			_delay_us(600);
-		else
-			_delay_us(3000);
+		if(TEACH())
+		{
+			drill(1000, 130, 80);
+		}
+
+		if(nap_pained)
+		{
+			if(KESKI())
+				_delay_us(600);
+			else
+				_delay_us(3000);
+		}
+
+		if(next_op == OP_MA)
+		{
+			move_xy(1, next_x, next_y);
+			wait_step_pause();
+			if(!in_pause)	print_string("OK\n\r");
+			next_op = OP_NOP;
+		}
+
+		if(next_op == OP_MR)
+		{
+			move_xy(0, next_x, next_y);
+			wait_step_pause();
+			if(!in_pause)	print_string("OK\n\r");
+			next_op = OP_NOP;
+		}
+
+		if(next_op == OP_DRI)
+		{
+			drill(next_x, next_y, next_z);
+			print_string("OK\n\r");
+			next_op = OP_NOP;
+		}
+
+		if(next_op == OP_FZ)
+		{
+			find_home_z();
+			print_string("OK\n\r");
+			next_op = OP_NOP;
+		}
+
+		if(next_op == OP_FH)
+		{
+			find_home_xy();
+			print_string("OK\n\r");
+			next_op = OP_NOP;
+		}
 
 	}
 
